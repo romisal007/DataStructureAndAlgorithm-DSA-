327af17c67b5fba7f75b385231fc7c117a97b050
Rohit Misal <50991203+romisal007@users.noreply.github.com>
2026-04-13 22:06:10 +0530

    Implement digit counting and string length display
    Add functionality to count digits of an integer and length of a string.

---
+++ maths/Count Digits.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;  
+
+class Main {
+    public static void main(String[] args) {
+      
+      Scanner sc = new Scanner(System.in);
+      String  a=sc.nextLine();
+      System.out.println(a.length());
+      
+      Scanner sc = new Scanner(System.in);
+      int  x=sc.nextInt();
+      int c=0;
+      while(x>0){
+          x=x/10;
+          c++;
+      }
+      System.out.println(c);      
+      
+    }
+}
