ec2b258dcbbe68018014fe694e1414eea80b3d1e
Rohit Misal <50991203+romisal007@users.noreply.github.com>
2026-04-13 23:16:35 +0530

    TrailZero

---
+++ maths/TrailZero.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+class TrailZero {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n= sc.nextInt();
+        System.out.println("Factorial of no:"+fact(n));
+        int f=fact(n);
+        System.out.println("No of trailing zeros:"+trailZero(f)); //works  upto n=12 as n increaces  factorraicl is greateg than limit of int so will give wrong ans use  efficTrailZero metho
+        System.out.println("efficTrailZero:"+efficTrailZero(n));
+       
+    }
+    static int fact(int n){
+     int fact=1;
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+    return fact;
+}
+    static int trailZero(int f){
+        int c=0;
+            while(f%10==0){
+                f=f/10;
+                c++;
+            }
+          return c;    
+     }
+     static int efficTrailZero(int f){
+        int z=0;
+        for(int i=5;i<=f;i=i*5){
+            z=z+f/i;
+        }
+    return z;
+     }
+}
